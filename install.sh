ce5f7d05c61ccb2cb1afa98b381dbd3f94e0d679
Glen Larsen <glenl.glx@gmail.com>
2026-05-16 16:52:28 -0700

    Commit after rename.

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #! /usr/bin/env bash
 
-stow emacs \
-    --verbose \
-    --ignore='.*\.elc' \
-    --ignore='.*\.org' \
-    --ignore='.*\.md' \
-    --ignore='Makefile' \
-    --target=$HOME \
+set -euo pipefail
+
+flags="\
+ --ignore='.*\.elc' \
+ --ignore='.*\.org' \
+ --ignore='.*\.md' \
+ --ignore=Makefile"
+
+# options
+while getopts v opts
+do
+    case $opts in
+        v) flags="$flags --verbose";;
+    esac
+done
+
+stow ${@:-emacs} $flags --target=$HOME
